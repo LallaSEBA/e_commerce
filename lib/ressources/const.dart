f297538233508bc2c2e5daeb63fef6b3bcbe2c5f
unknown <infomath12@gmail.com>
2021-05-18 10:22:03 +0100

    first commit

---
+++ lib/ressources/const.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+//fonts
+const String fnt_futura  = 'futura';
+const String fnt_futuraM = 'futuraMedium';
+
+//colors
+const Color color_BackgrndBlue = Color(0xff132e46);
+const Color color_Backgrnd   = Color(0xfff5fafd);
+const Color color_Text       = Color(0x8d6d7fb2);
+Color color_task             = Color(0xffb8cce6).withOpacity(0.20);
+const Color color_title      = Color(0xff0f2437);
+const Color color_topTitle   = Color(0xffc7e4ff);
+
+const Color color_drawerTitle   = Color(0xffe8f2fc);
+const Color color_drawerElem    = Color(0xffffffff);
+      Color color_drawerfocus   = Color(0xff47a8ff).withOpacity(0.11);
+const Color color_Btn           = Color(0xffffbc41);
+const Color color_txtBtn        = Color(0xff474747);
+const Color color_AppBarTitle   = Color(0xffeff5fa);
---
+++ lib/ressources/globals.dart
@@ -0,0 +1,4 @@
+
+String shared_token = 'Token';
+String shared_name  = 'Name';
+String shared_email = 'Email';
